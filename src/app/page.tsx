ac2c46a4a3a6ec341e4b7b90a66a969f4e300b63
Credios <gabriel.meirelles@credios.com.br>
2025-03-08 15:47:48 -0300

    correção page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import AdvantagesSection from "@/components/advantagessection";
 import HeroSection from "@/components/herosection";
 import SobreCredios from "@/components/sobrecredios";
 import Testemunhos from "@/components/testemunhos";
-import Image from "next/image";
 
 export default function Home() {
   return (
